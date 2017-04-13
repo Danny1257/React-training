512adb7c59314d55c6bddd04a7d53320e6dd7143
Daniel Hayes <dannyhayes57@hotmail.co.uk>
2017-04-13 17:32:25 +0100

    Basic user profile component

--- github-battle/app/index.js
+++ github-battle/app/index.js
@@ -1,18 +1,33 @@
 var React = require('react');
-var ReactDom = require('react-dom');
-require('./index.css');
+var ReactDOM = require('react-dom');
+var PropTypes = require('prop-types');
 
-class App extends React.Component {
+class Badge extends React.Component {
   render() {
     return (
       <div>
-        Hello React Training!
+        <img
+          src={this.props.img}
+          alt='Avatar'
+          style={{width: 100, height: 100}}
+        />
+        <h1>Name: {this.props.name}</h1>
+        <h3>username: {this.props.username}</h3>
       </div>
     )
   }
 }
 
-ReactDom.render(
-  <App />,
+Badge.propTypes = {
+  img: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired
+}
+
+ReactDOM.render(
+  <Badge
+    name='Tyler McGinnis'
+    username='tylermcginnis'
+    img={'https://avatars0.githubusercontent.com/u/2933430?v=3&s=460'} />,
   document.getElementById('app')
 );
